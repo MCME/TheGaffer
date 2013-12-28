147d3fc04b1dbf9ab7f831c0208fd773ab82ac1e
Henry Slawniak <meggawatts@gmail.com>
2013-12-27 21:00:13 -0600

    Add protection from certain item use

--- src/main/java/co/mcme/thegaffer/listeners/ProtectionListener.java
+++ src/main/java/co/mcme/thegaffer/listeners/ProtectionListener.java
@@ -21,14 +21,17 @@ import co.mcme.thegaffer.utilities.PermissionsUtil;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.hanging.HangingPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ProtectionListener implements Listener {
 
@@ -131,7 +134,7 @@ public class ProtectionListener implements Listener {
             }
         }
     }
-    
+
     @EventHandler
     public void onHangingBreak(HangingBreakByEntityEvent event) {
         if (event.getRemover() instanceof Player) {
@@ -184,7 +187,7 @@ public class ProtectionListener implements Listener {
             }
         }
     }
-    
+
     @EventHandler
     public void onHangingPlace(HangingPlaceEvent event) {
         Player player = (Player) event.getPlayer();
@@ -231,6 +234,94 @@ public class ProtectionListener implements Listener {
                 }
             } else {
                 player.sendMessage(ChatColor.DARK_RED + "You are not allowed to build when there are no jobs.");
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        boolean restricted = false;
+        Player player = (Player) event.getPlayer();
+        if (player.hasPermission(PermissionsUtil.getIgnoreWorldProtection())) {
+            event.setCancelled(false);
+        } else if (event.hasItem() && event.hasBlock()) {
+            if (event.getItem().getType().equals(Material.INK_SACK)) {
+                if (event.getItem().getData().getData() == 15
+                        && (event.getClickedBlock().getType() == Material.GRASS
+                        || event.getClickedBlock().getType() == Material.SAPLING
+                        || event.getClickedBlock().getType() == Material.CROPS
+                        || event.getClickedBlock().getType() == Material.BROWN_MUSHROOM
+                        || event.getClickedBlock().getType() == Material.RED_MUSHROOM
+                        || event.getClickedBlock().getType() == Material.PUMPKIN_STEM
+                        || event.getClickedBlock().getType() == Material.MELON_STEM
+                        || event.getClickedBlock().getType() == Material.POTATO
+                        || event.getClickedBlock().getType() == Material.CARROT
+                        || event.getClickedBlock().getType() == Material.COCOA)) {
+                    restricted = true;
+                } else if (event.getItem().getData().getData() == 3) {
+                    restricted = true;
+                }
+            }
+            if (event.getClickedBlock().getType().equals(Material.FLOWER_POT)) {
+                //cancelling this currently does nothing.
+                if (event.getItem().getType() == Material.RED_ROSE
+                        || event.getItem().getType() == Material.YELLOW_FLOWER
+                        || event.getItem().getType() == Material.SAPLING
+                        || event.getItem().getType() == Material.RED_MUSHROOM
+                        || event.getItem().getType() == Material.BROWN_MUSHROOM
+                        || event.getItem().getType() == Material.CACTUS
+                        || event.getItem().getType() == Material.LONG_GRASS
+                        || event.getItem().getType() == Material.DEAD_BUSH) {
+                    restricted = true;
+                }
+            }
+        }
+        if (restricted) {
+            World world = event.getClickedBlock().getWorld();
+            if (!JobDatabase.getActiveJobs().isEmpty()) {
+                HashMap<Job, World> workingworlds = new HashMap();
+                HashMap<Job, Rectangle2D> areas = new HashMap();
+                for (Job job : JobDatabase.getActiveJobs().values()) {
+                    workingworlds.put(job, job.getBukkitWorld());
+                    areas.put(job, job.getBounds());
+                }
+                if (workingworlds.containsValue(world)) {
+                    boolean playerisworking = false;
+                    for (Job job : workingworlds.keySet()) {
+                        if (job.isPlayerWorking(event.getPlayer())) {
+                            playerisworking = true;
+                        }
+                    }
+                    if (playerisworking) {
+                        boolean isinjobarea = false;
+                        int x = event.getClickedBlock().getX();
+                        int z = event.getClickedBlock().getZ();
+                        for (Job job : JobDatabase.getActiveJobs().values()) {
+                            if (job.isPlayerWorking(event.getPlayer()) && job.getBounds().contains(x, z)) {
+                                isinjobarea = true;
+                            }
+                        }
+                        if (isinjobarea) {
+                            event.setCancelled(false);
+                        } else {
+                            event.getPlayer().sendMessage(ChatColor.RED + "You have gone out of bounds for the job.");
+                            event.setUseItemInHand(Result.DENY);
+                            event.setCancelled(true);
+                        }
+                    } else {
+                        event.getPlayer().sendMessage(ChatColor.DARK_RED + "You are not part of any job.");
+                        event.setUseItemInHand(Result.DENY);
+                        event.setCancelled(true);
+                    }
+                } else {
+                    event.getPlayer().sendMessage(ChatColor.DARK_RED + "You are not allowed to build in this world.");
+                    event.setUseItemInHand(Result.DENY);
+                    event.setCancelled(true);
+                }
+            } else {
+                event.getPlayer().sendMessage(ChatColor.DARK_RED + "You are not allowed to build when there are no jobs.");
+                event.setUseItemInHand(Result.DENY);
                 event.setCancelled(true);
             }
         }
