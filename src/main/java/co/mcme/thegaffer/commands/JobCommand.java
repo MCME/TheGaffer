9e5a7edd4969d362f6c9fd3756486a7b0c509435
Donovan S. Allen <dono.allen@gmail.com>
2014-05-06 21:13:07 -0600

    Stage 1 working
    I think this will work

--- src/main/java/co/mcme/thegaffer/commands/JobCommand.java
+++ src/main/java/co/mcme/thegaffer/commands/JobCommand.java
@@ -284,29 +284,32 @@ public class JobCommand implements TabExecutor {
             }
             if(args[0].equalsIgnoreCase("admit")){
                 Job senderJob = JobDatabase.getJobWorking(player);
-                if(senderJob.getAdmitedWorkers().contains(player.getName())){
-                     if(args.length>1){
-                         Player obj = TheGaffer.getServerInstance().getPlayer(args[1]);
-                         if(obj != null){
-                             if(senderJob.isPlayerWorking(obj)&&!senderJob.getAdmitedWorkers().contains(obj.getName())){
-                                if(!senderJob.getTSchannel().equalsIgnoreCase("0")){
-                                    obj.teleport(senderJob.getWarp().toBukkitLocation());
-                                    senderJob.getAdmitedWorkers().add(obj.getName());
-                                    return true;
-                                }
-                             }
-                             player.sendMessage("That player can't be found!");
-                             return true;
-                         }
-                         player.sendMessage("That player can't be found!");
-                         return true;
-                     }
-                     return false;
-                 }else{
-                     player.sendMessage(ChatColor.RED + "You do not have permission.");
+                for(String worker : senderJob.getAdmitedWorkers()){
+                    player.sendMessage(worker);
+                }
+//                if(senderJob.getAdmitedWorkers().contains(player.getName())){
+//                     if(args.length>1){
+//                         Player obj = TheGaffer.getServerInstance().getPlayer(args[1]);
+//                         if(obj != null){
+//                             if(senderJob.isPlayerWorking(obj)&&!senderJob.getAdmitedWorkers().contains(obj.getName())){
+//                                if(!senderJob.getTSchannel().equalsIgnoreCase("0")){
+//                                    obj.teleport(senderJob.getWarp().toBukkitLocation());
+//                                    senderJob.getAdmitedWorkers().add(obj.getName());
+//                                    return true;
+//                                }
+//                             }
+//                             player.sendMessage("That player can't be found!");
+//                             return true;
+//                         }
+//                         player.sendMessage("That player can't be found!");
+//                         return true;
+//                     }
+//                     return false;
+//                 }else{
+//                     player.sendMessage(ChatColor.RED + "You do not have permission.");
                  }
                  return false;
-             }
+//             }
         }
         return false;
     }
